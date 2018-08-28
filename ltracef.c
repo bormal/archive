647ebfd4152048aec37e5fd1ac7d6d778f75f3ce
bormal <bormal.bbs@gmail.com>
2018-08-28 12:37:22 -0400

    __PRETTY_FUNCTION__ added

--- ltracef.c
+++ ltracef.c
@@ -9,3 +9,10 @@
 #define LTRACEF(...)
 #endif
 /*----------------------------------------*/
+
+//__PRETTY_FUNCTION__ prints full function signature with class name
+
+// All this variables are gcc special variables now, not preprocessor macros any more.
+
+// Read more
+// https://stackoverflow.com/questions/2518969/gcc-unicode-and-function

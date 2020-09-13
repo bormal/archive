8a5970a5f38353bc968bb118bf3abbfa7621f0b6
bormal <bormal.bbs@gmail.com>
2020-09-13 13:15:26 -0400

    Update trace.h

--- exercises/trace.h
+++ exercises/trace.h
@@ -1,14 +1,61 @@
+/* Print pair */
+template <typename T,typename S>
+ostream & operator << (ostream &os , const pair<T,S> &v) {
+    os << "(" ;
+    os << v.first << "," << v.second << ")" ;
+    return os ;
+}
+
+/* Print vector */
+template <typename T>
+ostream & operator << (ostream &os , const vector<T> &v) {
+    os << "[" ;
+    int sz = v.size() ;
+    for(int i = 0 ; i < sz ; ++i) {
+        os << v[i] ;
+        if(i!=sz-1)os << "," ;
+    }
+    os << "]\n" ;
+    return os ;
+}
+
+/* Print set */
+template <typename T>
+ostream & operator << (ostream &os , const set<T> &v) {
+    T last = *v.rbegin() ;
+    os << "[" ;
+    for(auto it : v) {
+        os << it  ;
+        if(it != last) os << "," ;
+    }
+    os << "]\n" ;
+    return os ;
+}
+
+/* Print Map */
+template <typename T,typename S>
+ostream & operator << (ostream &os , const map<T,S> &v) {
+    for(auto it : v) {
+        os << it.first << " : " << it.second << "\n" ;
+    }
+    return os ;
+}
+
 #define TRACE
 
 #ifdef TRACE
 #define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+
 template <typename Arg1>
-void __f(const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << std::endl; }
+void __f(const char* name, Arg1&& arg1) { 
+	cout << name << " : " << arg1 << std::endl; }
+
 template <typename Arg1, typename... Args>
 void __f(const char* names, Arg1&& arg1, Args&&... args) {
-		const char* comma = strchr(names + 1, ',');
-    cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+	const char* comma = strchr(names + 1, ',');
+	cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
 }
+
 #else
 #define trace(...)
 #endif
